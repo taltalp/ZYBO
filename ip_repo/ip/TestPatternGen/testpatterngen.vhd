570254e537180a9f29f5342d3a660a3bfe9d336d
taltalp <taltalpmh@gmail.com>
2017-10-20 14:01:06 +0900

    change testpatterngen documents

--- ip_repo/ip/TestPatternGen/testpatterngen.vhd
+++ ip_repo/ip/TestPatternGen/testpatterngen.vhd
@@ -1,36 +1,23 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 2017/06/16 00:16:50
--- Design Name: 
+-- Engineer: taltalp
+-- Create Date: 2017/06/16
+-- Design Name: VideoTestPatternGenerator
 -- Module Name: testpatterngen - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
 -- Description: 
--- 
--- Dependencies: 
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
+--
 -- Additional Comments:
 --  Video Timing Charts
 --  http://www.3dexpress.de/displayconfigx/timings.html
 ----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
 library UNISIM;
 use UNISIM.VComponents.all;
 
